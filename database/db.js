8bd13bcfc25bfdc4a129f1d7f25607fff59ba722
Nikola Bukvic <nb003522170@fer.hr>
2023-11-13 04:51:50 +0100

    Update db.js

--- database/db.js
+++ database/db.js
@@ -14,39 +14,34 @@ const pool = new Pool({
 
 // Clear the database
 const clearDatabase = async () => {
-  try {
-      // Delete data from the 'comments' table first to avoid foreign key constraints issues
-      await pool.query('DELETE FROM Comments;');
-      // Then delete data from the 'users' table
-      await pool.query('DELETE FROM Users;');
-      // Clear the 'session' table
-      await pool.query('DELETE FROM session;');
-      
-      // Reset the auto-incrementing IDs for both 'Users' and 'Comments' tables
-      await pool.query('ALTER SEQUENCE Users_user_id_seq RESTART WITH 1;');
-      await pool.query('ALTER SEQUENCE Comments_comment_id_seq RESTART WITH 1;');
-      
-      console.log('The database has been cleared.');
-  } catch (error) {
-      console.error('Error while clearing data from the database:', error);
-  }
+    try {
+        // Delete data from the 'comments' table first to avoid foreign key constraints issues
+        await pool.query('DELETE FROM Comments;');
+        // Then delete data from the 'users' table
+        await pool.query('DELETE FROM Users;');
+        
+        // Reset the auto-incrementing IDs for both tables
+        await pool.query('ALTER SEQUENCE Users_user_id_seq RESTART WITH 1;');
+        await pool.query('ALTER SEQUENCE Comments_comment_id_seq RESTART WITH 1;');
+        
+        console.log('The database has been cleared.');
+    } catch (error) {
+        console.error('Error while clearing data from the database:', error);
+    }
 };
 
 // Function to display all data from tables for debugging
 async function displayAllTables() {
-  try {
-      const userResults = await pool.query("SELECT * FROM Users");
-      console.log("Content of table Users:", userResults.rows);
-    
-      const commentResults = await pool.query("SELECT * FROM Comments");
-      console.log("Content of table Comments:", commentResults.rows);
-
-      const sessionResults = await pool.query("SELECT * FROM session");
-      console.log("Content of table session:", sessionResults.rows);
-    
-  } catch (error) {
-      console.error("Error while retrieving data from tables:", error);
-  }
+    try {
+        const userResults = await pool.query("SELECT * FROM Users");
+        console.log("Content of table Users:", userResults.rows);
+      
+        const commentResults = await pool.query("SELECT * FROM Comments");
+        console.log("Content of table Comments:", commentResults.rows);
+      
+    } catch (error) {
+        console.error("Error while retrieving data from tables:", error);
+    }
 }
 
 // Create the database
@@ -61,7 +56,7 @@ const createTables = async () => {
         );
       `);
       console.log('Table "Users" has been successfully created or updated!');
-
+  
       // Create the 'comments' table
       await pool.query(`
         CREATE TABLE IF NOT EXISTS Comments (
@@ -72,23 +67,10 @@ const createTables = async () => {
         );
       `);
       console.log('Table "Comments" has been successfully created or updated!');
-
-      // Create the 'session' table
-      await pool.query(`
-            CREATE TABLE IF NOT EXISTS "session" (
-                "sid" varchar NOT NULL COLLATE "default",
-                "sess" json NOT NULL,
-                "expire" timestamp(6) NOT NULL,
-                PRIMARY KEY ("sid")
-            )
-            WITH (OIDS=FALSE);
-      `);
-      console.log('Table "session" has been successfully created or updated!');
     } catch (error) {
       console.error('Error while creating or updating tables:', error);
     }
 };
-
 
 module.exports = {
     pool,
